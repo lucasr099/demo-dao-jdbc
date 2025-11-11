ac5695d131594b80bd48f455e70f01b1f21f5676
Lucas Costa <lucasreiiss100@hotmail.com>
2025-11-11 12:17:58 -0300

    SellerDaoJDBC e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entites.Department;
 import model.entites.Seller;
 
@@ -9,6 +11,9 @@ public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1,"Books");
         Seller seller = new Seller(21,"bob", "bob@gmail.com",new Date(),3000.0,obj);
+
+        SellerDao sellerDao = DaoFactory.creatSellerDao();
+
         System.out.println(seller);
 
     }
